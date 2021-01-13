ba112fdf4032062dc6929b7abba19281cd3cd87a
Shauray-567 <shauryap92.com>
2021-01-13 20:24:45 +0530

    oslocommit

---
+++ index.js
@@ -0,0 +1,53 @@
+var slideIndex = 0;
+showSlides();
+
+
+
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) 
+  {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); 
+}
+
+var slideIndex12= 1;
+showSlides23(slideIndex12);
+function plusSlides(n) {
+  showSlides23(slideIndex += n)
+
+}
+
+function currentSlide(n) {
+ showSlides23(slideIndex = n);
+}
+
+function showSlides23(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length)
+   {
+     slideIndex = 1
+    }
+  if (n < 1)
+   {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) 
+  {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) 
+  {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
