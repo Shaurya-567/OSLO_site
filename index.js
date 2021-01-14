3a2b3dd1605848eb2d64c1b2ec735d004af221fa
Shaurya-567 <55078046+Shaurya-567@users.noreply.github.com>
2021-01-14 09:13:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) 
   {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) 
+  {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); 
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); 
 }
 
 var slideIndex12= 1;
